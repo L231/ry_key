f617a4dc79207d223b07c6c05f4ed39d424a96fe
L231 <111673693+L231@users.noreply.github.com>
2023-10-14 22:45:47 +0800

    现在支持长按连续触发，同时按键状态精简成两个

--- ry_key.c
+++ ry_key.c
@@ -7,15 +7,23 @@
 
 #include "ry_key.h"
 
+/*
+ * 控制逻辑
+ *
+ *  1. 组合键匹配成功，则不执行其余独立按键的事件，只响应组合键
+ *  2. 组合键匹配的必要条件，至少有一个关联按键触发了 KEY_DOWN_EVENT，即按下事件
+ *  3. 组合键匹配失败，则响应所有独立按键触发的事件
+ *
+ */
+
 
 
 /* 按键控制块，主要服务于组合键 */
 static ry_key_obj_t __keyObj =
 {
-		.sn                 = 0,
-		.event_cnt          = 0,
-		.down_key_cnt       = 0,
-		.current_active_key = RY_NULL,
+        .sn                 = 0,
+        .event_down_cnt     = 0,
+        .down_key_cnt       = 0,
 };
 /* 独立按键的单向链表，用于遍历 */
 static ry_slist_t __keySlist         = {.next = RY_NULL};
@@ -26,203 +34,206 @@ static ry_slist_t __keyCompoundSlist = {.next = RY_NULL};
 
 
 void ry_key_reg(ry_key_t *key,
-		uint8_t valid_level,         /* 有效电平，按键激活判断 */
+        uint8_t valid_level,         /* 有效电平，按键激活判断 */
+        uint8_t keep_trigger,        /* 长按连续触发 */
 #if KEY_LIMIT_DEFAULT_ENABLE == 0
-		uint8_t filter,              /* 电平滤波阈值 */
-		uint8_t double_click_limit,  /* 双击时间的阈值 */
-		uint8_t long_limit,          /* 长按的阈值 */
-		uint8_t long_long_limit,     /* 超长按的阈值 */
+        uint8_t filter,              /* 电平滤波阈值 */
+        uint8_t double_click_limit,  /* 双击时间的阈值 */
+        uint8_t long_limit,          /* 长按的阈值 */
+        uint8_t long_long_limit,     /* 超长按的阈值 */
 #endif
-		uint8_t (*get_level)(void))
+        uint8_t (*get_level)(void))
 {
-	uint8_t pos;
-	key->type                    = KEY_SINGLE_TYPE;
-	key->status                  = KEY_IDLE_STATUS;
-	key->event                   = KEY_NONE_EVENT;
-	key->sn                      = __keyObj.sn++;
-	key->scan.click_cnt          = 0;
-	key->scan.level              = !valid_level;
-	key->scan.valid_level        = valid_level;
-	key->scan.filter_cnt         = 0;
-	key->scan.tick               = 0;
+    uint8_t pos;
+    key->type                    = KEY_SINGLE_TYPE;
+    key->status                  = KEY_UP_STATUS;
+    key->event                   = KEY_NONE_EVENT;
+    key->sn                      = __keyObj.sn++;
+    key->long_press_keep_trigger = keep_trigger;
+    key->scan.click_cnt          = 0;
+    key->scan.level              = !valid_level;
+    key->scan.valid_level        = valid_level;
+    key->scan.filter_cnt         = 0;
+    key->scan.tick               = 0;
 #if KEY_LIMIT_DEFAULT_ENABLE == 0
-	key->scan.filter_limit       = filter;
-	key->scan.double_click_limit = double_click_limit;
-	key->scan.long_limit         = long_limit;
-	key->scan.long_long_limit    = long_long_limit;
+    key->scan.filter_limit       = filter;
+    key->scan.double_click_limit = double_click_limit;
+    key->scan.long_limit         = long_limit;
+    key->scan.long_long_limit    = long_long_limit;
 #endif
-	key->scan.get_level          = get_level;
-	for(pos = 0; pos < KEY_NONE_EVENT; pos++)
-		key->callback[pos] = RY_NULL;
-	ry_slist_init(&key->slist);
-	ry_slist_insert_after(&__keySlist, &key->slist);
+    key->scan.get_level          = get_level;
+    for(pos = 0; pos < KEY_NONE_EVENT; pos++)
+        key->callback[pos] = RY_NULL;
+    ry_slist_init(&key->slist);
+    ry_slist_insert_after(&__keySlist, &key->slist);
 }
 
 
 
+/* 组合键的注册函数 */
 void ry_key_compound_reg(ry_key_compound_t *key, callback cbk)
 {
-	key->type                 = KEY_COMPOUND_TYPE;
-	key->status               = KEY_IDLE_STATUS;
-	key->event                = KEY_NONE_EVENT;
-	key->key_num              = 0;
-	key->callback[0]          = cbk;
-	ry_slist_init(&key->slist);
-	ry_slist_insert_after(&__keyCompoundSlist, &key->slist);
+    key->type                 = KEY_COMPOUND_TYPE;
+    key->status               = KEY_UP_STATUS;
+    key->event                = KEY_NONE_EVENT;
+    key->key_num              = 0;
+    key->callback[0]          = cbk;
+    ry_slist_init(&key->slist);
+    ry_slist_insert_after(&__keyCompoundSlist, &key->slist);
 }
 
 
 /* 组合键添加关联的独立按键SN号，以SN大小插入 */
-void ry_key_compound_insert_key_sn(ry_key_compound_t *key, uint8_t sn)
+void ry_key_compound_insert_key_sn(ry_key_compound_t *key, ry_key_t *k)
 {
-	uint8_t pos;
-	uint8_t temp;
-	if(key->key_num >= KEY_COMPOUND_NUM)
-		return;
-	if(0 == key->key_num)
-	{
-		key->key_sn[0] = sn;
-	}
-	else
-	{
-		/* 从大到小排列 */
-		for(pos = 0; pos < key->key_num; pos++)
-		{
-			/* 重复添加某个按键 */
-			if(sn == key->key_sn[pos])
-				return;
-			if(sn > key->key_sn[pos])
-				break;
-		}
-		for(temp = key->key_num; temp > pos; temp--)
-			key->key_sn[temp] = key->key_sn[temp - 1];
-		key->key_sn[pos] = sn;
-	}
-	key->key_num++;
+    uint8_t pos;
+    uint8_t temp;
+    if(key->key_num >= KEY_COMPOUND_NUM)
+        return;
+    if(0 == key->key_num)
+    {
+        key->key_sn[0] = k->sn;
+    }
+    else
+    {
+        /* 从大到小排列 */
+        for(pos = 0; pos < key->key_num; pos++)
+        {
+            /* 重复添加某个按键 */
+            if(k->sn == key->key_sn[pos])
+                return;
+            if(k->sn > key->key_sn[pos])
+                break;
+        }
+        for(temp = key->key_num; temp > pos; temp--)
+            key->key_sn[temp] = key->key_sn[temp - 1];
+        key->key_sn[pos] = k->sn;
+    }
+    key->key_num++;
 }
 
 
 
-/* 标记当前的事件，并转移状态 */
+/* 标记当前的事件 */
 static void __key_event_mark(ry_key_t *key, uint8_t event)
 {
-	key->event = event;
-	if(KEY_NONE_EVENT == event)
-		return;
-	/* 累加所有按键的有效事件 */
-	__keyObj.current_active_key = key;
-	__keyObj.event_cnt++;
-	if(KEY_DOWN_EVENT == event)
-	{
-		__keyObj.event_down_cnt++;
-	}
+    key->event = event;
+    /* 记录非连续触发的按下事件个数 */
+    if(KEY_DOWN_EVENT == event && key->scan.tick == 0)
+    {
+        __keyObj.event_down_cnt++;
+    }
 }
 
-
+/* 记录按键按下的个数 */
 static void __key_down_mark(ry_key_t *key)
 {
-	if(KEY_DOWN_STATUS == key->status)
-	{
-		if(__keyObj.down_key_cnt < KEY_COMPOUND_NUM)
-		{
-			/* key链表是单向链表，注册时，其sn是从大到小 */
-			__keyObj.down_key_sn_buf[__keyObj.down_key_cnt] = key->sn;
-		}
-		__keyObj.down_key_cnt++;
-	}
+    if(KEY_DOWN_STATUS == key->status)
+    {
+        if(__keyObj.down_key_cnt < KEY_COMPOUND_NUM)
+        {
+            /* key链表是单向链表，注册时，其sn是从大到小 */
+            __keyObj.down_key_sn_buf[__keyObj.down_key_cnt] = key->sn;
+        }
+        __keyObj.down_key_cnt++;
+        
+    }
 }
 
 
 /* 调用按键相关的事件处理函数 */
 void ry_key_callback(ry_key_t *key, uint8_t event)
 {
-	if(KEY_NONE_EVENT != event && RY_NULL != key->callback[event])
-		key->callback[event](key);
-	key->event = KEY_NONE_EVENT;
+    if(KEY_NONE_EVENT != event && RY_NULL != key->callback[event])
+        key->callback[event](key);
+    key->event = KEY_NONE_EVENT;
 }
 
 
 /* 独立按键电平扫描 */
 void __key_level_scan(ry_key_t *key)
 {
-	uint8_t level = key->scan.get_level();
-	if(level != key->scan.level)
-	{
-		if(key->scan.filter_cnt++ >= __KEY_LEVEL_FILTER_LIMIT)
-		{
-			key->scan.level      = level;
-			key->scan.filter_cnt = 0;
-		}
-	}
-	else
-		key->scan.filter_cnt     = 0;
+    uint8_t level = key->scan.get_level();
+    if(level != key->scan.level)
+    {
+        if(++key->scan.filter_cnt >= __KEY_LEVEL_FILTER_LIMIT)
+        {
+            key->scan.level      = level;
+            key->scan.filter_cnt = 0;
+        }
+    }
+    else
+        key->scan.filter_cnt     = 0;
 }
 
 
 /* 独立按键的状态机 */
 uint8_t ry_key_state_machine(ry_key_t *key)
 {
-	__key_level_scan(key);
-	key->scan.tick++;
+    __key_level_scan(key);
+    /* 计时大于超长按阈值，就不要累加时间了 */
+    if(key->scan.tick <= __KEY_LONG_LONG_LIMIT)
+        key->scan.tick++;
 
-	switch(key->status)
-	{
-	case KEY_IDLE_STATUS :
-		if(key->scan.level == key->scan.valid_level)
-		{
-			__key_event_mark(key, KEY_DOWN_EVENT);
-			key->status         = KEY_DOWN_STATUS;
-			key->scan.tick      = 0;
-			key->scan.click_cnt = 0;
-		}
-		break;
-	case KEY_DOWN_STATUS :
-		if(key->scan.level != key->scan.valid_level)
-		{
-			__key_event_mark(key, KEY_UP_EVENT);
-			key->status         = KEY_UP_STATUS;
-			if(key->scan.tick < __KEY_DOUBLE_CLICK_LIMIT)
-				key->scan.click_cnt++;
-		}
-		else if(key->scan.tick == __KEY_LONG_LIMIT)
-		{
-			__key_event_mark(key, KEY_LONG_PRESS_EVENT);
-			key->scan.click_cnt = 0;
-		}
-		else if(key->scan.tick == __KEY_LONG_LONG_LIMIT)
-		{
-			__key_event_mark(key, KEY_LONG_LONG_PRESS_EVENT);
-			key->scan.click_cnt = 0;
-#if KEY_LONG_LONG_PRESS_STATUS_KEEP == 0
-			key->status         = KEY_IDLE_STATUS;
-#endif
-		}
-		break;
-	case KEY_UP_STATUS :
-		if(key->scan.level == key->scan.valid_level)
-		{
-			__key_event_mark(key, KEY_DOWN_EVENT);
-			key->status         = KEY_DOWN_STATUS;
-			key->scan.tick = 0;
-		}
-		/* 从按键按下开始计时，若时间超过连击时间阈值，则认为连击结束 */
-		else if(key->scan.tick > __KEY_DOUBLE_CLICK_LIMIT)
-		{
-			if(1 == key->scan.click_cnt)
-				__key_event_mark(key, KEY_SINGLE_CLICK_EVENT);
-			else if(2 == key->scan.click_cnt)
-				__key_event_mark(key, KEY_DOUBLE_CLICK_EVENT);
-			else if(3 == key->scan.click_cnt)
-				__key_event_mark(key, KEY_THREE_CLICK_EVENT);
-			key->status         = KEY_IDLE_STATUS;
-		}
-		break;
-	default :
-		key->status             = KEY_IDLE_STATUS;
-		break;
-	}
-	__key_down_mark(key);
-	return key->event;
+    switch(key->status)
+    {
+    case KEY_DOWN_STATUS :
+        /* 弹起事件 */
+        if(key->scan.level != key->scan.valid_level)
+        {
+            __key_event_mark(key, KEY_UP_EVENT);
+            key->status         = KEY_UP_STATUS;
+            /* 记录连击次数 */
+            if(key->scan.tick < __KEY_DOUBLE_CLICK_LIMIT)
+                key->scan.click_cnt++;
+        }
+        /* 时基 >= 长按连续触发的阈值 */
+        else if(key->scan.tick >= __KEY_LONG_PRESS_KEEP_TRIGGER_LIMIT)
+        {
+            /* 连续触发 */
+            if(key->long_press_keep_trigger)
+            {
+                __key_event_mark(key, KEY_DOWN_EVENT);
+                /* 时基清零，保证周期性触发 */
+                key->scan.tick  = 0;
+            }
+            /* 长按事件 */
+            else if(key->scan.tick == __KEY_LONG_LIMIT)
+                __key_event_mark(key, KEY_LONG_PRESS_EVENT);
+            /* 超长按事件 */
+            else if(key->scan.tick == __KEY_LONG_LONG_LIMIT)
+                __key_event_mark(key, KEY_LONG_LONG_PRESS_EVENT);
+            /* 长按，则连击事件应该消除 */
+            key->scan.click_cnt = 0;
+        }
+        break;
+    case KEY_UP_STATUS :
+        /* 按下事件 */
+        if(key->scan.level == key->scan.valid_level)
+        {
+            /* 先清零，表明是非连续触发的按下事件 */
+            key->scan.tick      = 0;
+            __key_event_mark(key, KEY_DOWN_EVENT);
+            key->status         = KEY_DOWN_STATUS;
+        }
+        /* 从按键按下开始计时，若时间超过连击时间阈值，则认为连击结束 */
+        else if(key->scan.tick > __KEY_DOUBLE_CLICK_LIMIT && key->scan.click_cnt > 0)
+        {
+            if(1 == key->scan.click_cnt)
+                __key_event_mark(key, KEY_SINGLE_CLICK_EVENT);
+            else if(2 == key->scan.click_cnt)
+                __key_event_mark(key, KEY_DOUBLE_CLICK_EVENT);
+            else if(3 == key->scan.click_cnt)
+                __key_event_mark(key, KEY_THREE_CLICK_EVENT);
+            key->scan.click_cnt = 0;
+        }
+        break;
+    default :
+        key->status             = KEY_UP_STATUS;
+        break;
+    }
+    __key_down_mark(key);
+    return key->event;
 }
 
 
@@ -230,70 +241,71 @@ uint8_t ry_key_state_machine(ry_key_t *key)
 /* 按键扫描，优先响应组合键，独立按键次之 */
 void ry_key_scan(void)
 {
-	uint8_t pos;
-	uint8_t Error;
-	ry_key_compound_t *KeyCompound;
-	ry_slist_t *Node;
+    uint8_t pos;
+    uint8_t FlagKeyCompound = 0;
+    ry_key_t          *Key;
+    ry_key_compound_t *KeyCompound;
+    ry_slist_t *Node;
 
-	/* 扫描所有的独立物理按键 */
-	for(Node = __keySlist.next; Node != RY_NULL; Node = Node->next)
-	{
-		ry_key_state_machine(TASK_CONTAINER_OF(Node, ry_key_t, slist));
-	}
+    /* 扫描所有的独立物理按键 */
+    for(Node = __keySlist.next; Node != RY_NULL; Node = Node->next)
+    {
+        ry_key_state_machine(TASK_CONTAINER_OF(Node, ry_key_t, slist));
+    }
 
-	/* 过多按键激活 */
-	if(__keyObj.event_cnt > KEY_COMPOUND_NUM || __keyObj.down_key_cnt > KEY_COMPOUND_NUM)
-		goto __KEY_EVENT_CLEAR;
-	/* 多个按键触发了不同的事件 */
-	if(__keyObj.event_cnt > 1 && __keyObj.event_cnt != __keyObj.event_down_cnt)
-		goto __KEY_EVENT_CLEAR;
+    /* 过多按键激活 */
+    if(__keyObj.down_key_cnt > KEY_COMPOUND_NUM)
+        goto __KEY_EVENT_CLEAR;
 
-	/* 需要判断组合键，高优先级。有多个独立按键激活，同时按下事件数不为0，没有发生其它事件 */
-	/* 存在小概率事件，即组合键本应匹配成功，但因为其中某个关联按键的长按事件触发了，导致误判为事件冲突 */
-	if(__keyObj.down_key_cnt > 1 &&
-		__keyObj.event_down_cnt > 0 &&
-		__keyObj.event_cnt == __keyObj.event_down_cnt)
-	{
-		for(Node = __keyCompoundSlist.next; Node != RY_NULL; Node = Node->next)
-		{
-			KeyCompound = TASK_CONTAINER_OF(Node, ry_key_compound_t, slist);
-			/* 组合键的按键个数与激活的按键个数匹配 */
-			/* 该方案无组合键包含冲突，个数越多优先级越高 */
-			if(KeyCompound->key_num == __keyObj.down_key_cnt)
-			{
-				Error = 0;
-				for(pos = 0; pos < __keyObj.down_key_cnt; pos++)
-				{
-					/* 两个数组缓存的sn，都是递减排列 */
-					if(__keyObj.down_key_sn_buf[pos] != KeyCompound->key_sn[pos])
-					{
-						Error = 1;
-						break;
-					}
-				}
-				/* 组合键识别到了 */
-				if(0 == Error)
-				{
-					ry_key_callback((ry_key_t *)KeyCompound, KEY_COMPOUND_EVENT);
-					break;
-				}
-			}
-		}
-	}
-	/* 处理独立按键的事件，低优先级。必须要有一个事件，且已按下的按键数 <= 1 */
-	else if(1 == __keyObj.event_cnt && __keyObj.down_key_cnt <= 1)
-	{
-		/* 在上述条件下，如果当前按键没有按下，则已按下的按键数必须为0 */
-		if(KEY_DOWN_STATUS != __keyObj.current_active_key->status && 1 == __keyObj.down_key_cnt)
-			goto __KEY_EVENT_CLEAR;
-		ry_key_callback(__keyObj.current_active_key, __keyObj.current_active_key->event);
-	}
-
+    /* 匹配组合键，成功则不执行余下的独立按键的事件 */
+    if(__keyObj.down_key_cnt > 1 && __keyObj.event_down_cnt > 0)
+    {
+        for(Node = __keyCompoundSlist.next; Node != RY_NULL; Node = Node->next)
+        {
+            KeyCompound = TASK_CONTAINER_OF(Node, ry_key_compound_t, slist);
+            if(KeyCompound->key_num != __keyObj.down_key_cnt)
+                continue;
+            /* 组合键的按键个数与激活的按键个数匹配 */
+            FlagKeyCompound = 1;
+            for(pos = 0; pos < __keyObj.down_key_cnt; pos++)
+            {
+                /* 两个数组缓存的sn，都是递减排列 */
+                if(__keyObj.down_key_sn_buf[pos] != KeyCompound->key_sn[pos])
+                {
+                    FlagKeyCompound = 0;
+                    break;
+                }
+            }
+            /* 组合键识别到了 */
+            if(1 == FlagKeyCompound)
+            {
+                ry_key_callback((ry_key_t *)KeyCompound, KEY_COMPOUND_EVENT);
+                break;
+            }
+        }
+    }
+    
+    /* 扫描所有的独立物理按键，执行已触发的事件 */
+    if(0 == FlagKeyCompound)
+    {
+        for(Node = __keySlist.next; Node != RY_NULL; Node = Node->next)
+        {
+            Key = TASK_CONTAINER_OF(Node, ry_key_t, slist);
+            ry_key_callback(Key, Key->event);
+        }
+    }
+    else
+    {
+        /* 清除所有独立按键的事件 */
 __KEY_EVENT_CLEAR :
-	__keyObj.event_cnt          = 0;
-	__keyObj.event_down_cnt     = 0;
-	__keyObj.down_key_cnt       = 0;
-	__keyObj.current_active_key = RY_NULL;
+        for(Node = __keySlist.next; Node != RY_NULL; Node = Node->next)
+        {
+            TASK_CONTAINER_OF(Node, ry_key_t, slist)->event = KEY_NONE_EVENT;
+        }
+    }
+
+    __keyObj.event_down_cnt     = 0;
+    __keyObj.down_key_cnt       = 0;
 }
 
 
--- ry_key.h
+++ ry_key.h
@@ -15,54 +15,64 @@
 
 
 
-/* 按键持续按下，则保持超长按状态 */
-#define KEY_LONG_LONG_PRESS_STATUS_KEEP           1
 
 /* 独立按键扫描的阈值是否采用默认值 */
 #define KEY_LIMIT_DEFAULT_ENABLE                  1
 
+/* 独立按键的电平扫描周期，单位：ms */
+#define KEY_SCAN_CYCLE                           (10)
 
-
+/* 阈值（需自行调整），单位：系统时基 */
 #if KEY_LIMIT_DEFAULT_ENABLE == 1
-#define __KEY_LEVEL_FILTER_LIMIT                  5
-#define __KEY_DOUBLE_CLICK_LIMIT                  150
-#define __KEY_LONG_LIMIT                          2000
-#define __KEY_LONG_LONG_LIMIT                     5000
+/* 电平滤波阈值 */
+#define __KEY_LEVEL_FILTER_LIMIT                 (20   / KEY_SCAN_CYCLE)
+/* 双击的阈值，大于该值则结束双击判断 */
+#define __KEY_DOUBLE_CLICK_LIMIT                 (300  / KEY_SCAN_CYCLE)
+/* 长按的阈值 */
+#define __KEY_LONG_LIMIT                         (2000 / KEY_SCAN_CYCLE)
+/* 超长按的阈值 */
+#define __KEY_LONG_LONG_LIMIT                    (5000 / KEY_SCAN_CYCLE)
 #else
 #define __KEY_LEVEL_FILTER_LIMIT                  key->scan.filter_limit
 #define __KEY_DOUBLE_CLICK_LIMIT                  key->scan.double_click_limit
 #define __KEY_LONG_LIMIT                          key->scan.long_limit
 #define __KEY_LONG_LONG_LIMIT                     key->scan.long_long_limit
 #endif
+/* 长按周期性触发的时基阈值 */
+#define __KEY_LONG_PRESS_KEEP_TRIGGER_LIMIT      (__KEY_DOUBLE_CLICK_LIMIT)
 
 
+
+/* 组合键最大关联的独立按键数 */
 #define KEY_COMPOUND_NUM               5
 
 typedef enum
 {
-	KEY_DOWN_STATUS,             /* 按下状态 */
-	KEY_UP_STATUS,               /* 弹起状态 */
-	KEY_IDLE_STATUS,             /* 空闲状态 */
+    KEY_DOWN_STATUS,             /* 按下状态 */
+    KEY_UP_STATUS,               /* 弹起状态 */
 }E_key_status_t;
 
 typedef enum
 {
-	KEY_COMPOUND_EVENT,        /* 组合键事件 */
-	KEY_DOWN_EVENT,            /* 按下事件 */
-	KEY_UP_EVENT,              /* 弹起事件 */
-	KEY_SINGLE_CLICK_EVENT,    /* 单击事件 */
-	KEY_DOUBLE_CLICK_EVENT,    /* 双击事件 */
-	KEY_THREE_CLICK_EVENT,     /* 三击事件 */
-	KEY_LONG_PRESS_EVENT,      /* 长按事件 */
-	KEY_LONG_LONG_PRESS_EVENT, /* 超长按事件 */
-	KEY_NONE_EVENT,            /* 无 */
+    KEY_COMPOUND_EVENT,        /* 组合键事件 */
+    KEY_DOWN_EVENT,            /* 按下事件 */
+    KEY_UP_EVENT,              /* 弹起事件 */
+    KEY_SINGLE_CLICK_EVENT,    /* 单击事件 */
+    KEY_DOUBLE_CLICK_EVENT,    /* 双击事件 */
+    KEY_THREE_CLICK_EVENT,     /* 三击事件 */
+    KEY_LONG_PRESS_EVENT,      /* 长按事件 */
+    KEY_LONG_LONG_PRESS_EVENT, /* 超长按事件 */
+    KEY_NONE_EVENT,            /* 无 */
 }E_key_event_t;
 
 typedef enum
 {
-	KEY_SINGLE_TYPE,
-	KEY_COMPOUND_TYPE,
-}E_key_type_t;
+    KEY_SINGLE_TYPE,                  /* 独立按键 */
+    KEY_COMPOUND_TYPE,                /* 组合键 */
+    
+    KEY_LONG_PRESS_NO_KEEP_TRIG = 0,  /* 不开启长按连续触发 */
+    KEY_LONG_PRESS_KEEP_TRIG,         /* 长按连续触发模式 */
+}E_key_parameter_t;
 
 
 typedef struct ry_key ry_key_t;
@@ -72,56 +82,56 @@ typedef void (*callback)(ry_key_t* key);
 /* 独立按键扫描参数的结构体 */
 typedef struct
 {
-	uint8_t                 level       : 1;                   /* 当前电平 */
-	uint8_t                 valid_level : 1;                   /* 有效电平，按键激活判断 */
-	uint8_t                 reserve     : 2;                   /* 保留 */
-	uint8_t                 click_cnt   : 4;                   /* 连击次数 */
-	uint8_t                 filter_cnt;                        /* 电平滤波计数 */
+    uint8_t                 level                   : 1;       /* 当前电平 */
+    uint8_t                 valid_level             : 1;       /* 有效电平，按键激活判断 */
+    uint8_t                 reserve                 : 2;       /* 保留 */
+    uint8_t                 click_cnt               : 4;       /* 连击次数 */
+    uint8_t                 filter_cnt;                        /* 电平滤波计数 */
 #if KEY_LIMIT_DEFAULT_ENABLE == 0
-	uint8_t                 filter_limit;                      /* 电平滤波阈值 */
+    uint8_t                 filter_limit;                      /* 电平滤波阈值 */
 #endif
-	uint16_t                tick;                              /* 按键扫描计时 */
+    uint16_t                tick;                              /* 按键扫描计时 */
 #if KEY_LIMIT_DEFAULT_ENABLE == 0
-	uint16_t                double_click_limit;                /* 双击时间的阈值 */
-	uint16_t                long_limit;                        /* 长按的阈值 */
-	uint16_t                long_long_limit;                   /* 超长按的阈值 */
+    uint16_t                double_click_limit;                /* 双击时间的阈值 */
+    uint16_t                long_limit;                        /* 长按的阈值 */
+    uint16_t                long_long_limit;                   /* 超长按的阈值 */
 #endif
-	uint8_t               (*get_level)(void);                  /* 电平获取，函数指针 */
+    uint8_t               (*get_level)(void);                  /* 电平获取，函数指针 */
 }key_scan_t;
 
 /* 组合键的控制块 */
 typedef struct
 {
-	uint8_t                 type        : 1;
-	uint8_t                 status      : 3;
-	uint8_t                 event       : 4;
-	ry_slist_t              slist;
-	callback                callback[1];
-	uint8_t                 key_num;                           /* 关联的独立按键个数 */
-	uint8_t                 key_sn[KEY_COMPOUND_NUM];          /* 关联的独立按键的SN号 */
+    uint8_t                 type                    : 1;
+    uint8_t                 long_press_keep_trigger : 1;       /* 长按保持触发 */
+    uint8_t                 status                  : 2;
+    uint8_t                 event                   : 4;
+    ry_slist_t              slist;
+    callback                callback[1];
+    uint8_t                 key_num;                           /* 关联的独立按键个数 */
+    uint8_t                 key_sn[KEY_COMPOUND_NUM];          /* 关联的独立按键的SN号 */
 }ry_key_compound_t;
 
 /* 独立按键的控制块 */
 struct ry_key
 {
-	uint8_t                 type        : 1;
-	uint8_t                 status      : 3;
-	uint8_t                 event       : 4;
-	ry_slist_t              slist;
-	callback                callback[KEY_NONE_EVENT];          /* 事件的回调函数 */
-	uint8_t                 sn;                                /* 与注册到链表的先后序号对应 */
-	key_scan_t              scan;                              /* 独立按键扫描参数的结构体 */
+    uint8_t                 type                    : 1;
+    uint8_t                 long_press_keep_trigger : 1;       /* 长按连续触发 */
+    uint8_t                 status                  : 2;
+    uint8_t                 event                   : 4;
+    ry_slist_t              slist;
+    callback                callback[KEY_NONE_EVENT];          /* 事件的回调函数 */
+    uint8_t                 sn;                                /* 与注册到链表的先后序号对应 */
+    key_scan_t              scan;                              /* 独立按键扫描参数的结构体 */
 };
 
 
 typedef struct
 {
-	uint8_t                 sn;                                /* 独立按键链表的下标，作为按键的SN号 */
-	uint8_t                 event_cnt;                         /* 记录有多少事件触发 */
-	uint8_t                 event_down_cnt;                    /* 记录有多少按压类事件触发 */
-	uint8_t                 down_key_cnt;                      /* 记录有多少个按键按下 */
-	uint8_t                 down_key_sn_buf[KEY_COMPOUND_NUM]; /* 缓存已按下的按键的SN号 */
-	ry_key_t               *current_active_key;                /* 当前激活的按键 */
+    uint8_t                 sn;                                /* 独立按键链表的下标，作为按键的SN号 */
+    uint8_t                 event_down_cnt;                    /* 记录有多少按压类事件触发 */
+    uint8_t                 down_key_cnt;                      /* 记录有多少个按键按下 */
+    uint8_t                 down_key_sn_buf[KEY_COMPOUND_NUM]; /* 缓存已按下的按键的SN号 */
 }ry_key_obj_t;
 
 
@@ -130,18 +140,22 @@ typedef struct
 #define RY_KEY_SET_EVENT_HANDLER(key, event, cbk)       key.callback[event] = cbk
 
 extern void ry_key_reg(ry_key_t *key,
-		uint8_t valid_level,         /* 有效电平，按键激活判断 */
+        uint8_t valid_level,         /* 有效电平，按键激活判断 */
+        uint8_t keep_trigger,        /* 长按连续触发 */
 #if KEY_LIMIT_DEFAULT_ENABLE == 0
-		uint8_t filter,              /* 电平滤波阈值 */
-		uint8_t double_click_limit,  /* 双击时间的阈值 */
-		uint8_t long_limit,          /* 长按的阈值 */
-		uint8_t long_long_limit,     /* 超长按的阈值 */
+        uint8_t filter,              /* 电平滤波阈值 */
+        uint8_t double_click_limit,  /* 双击时间的阈值 */
+        uint8_t long_limit,          /* 长按的阈值 */
+        uint8_t long_long_limit,     /* 超长按的阈值 */
 #endif
-		uint8_t (*get_level)(void));
+        uint8_t (*get_level)(void));
+/* 组合键的注册函数 */
 extern void ry_key_compound_reg(ry_key_compound_t *key, callback cbk);
 
-extern void ry_key_compound_insert_key_sn(ry_key_compound_t *key, uint8_t sn);
+/* 组合键添加关联的独立按键SN号，以SN大小插入 */
+extern void ry_key_compound_insert_key_sn(ry_key_compound_t *key, ry_key_t *k);
 
+/* 调用按键事件的回调函数 */
 extern void ry_key_callback(ry_key_t *key, uint8_t event);
 extern uint8_t ry_key_state_machine(ry_key_t *key);
 
